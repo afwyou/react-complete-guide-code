e19b91def760a126db3d410932d845a940de7792
Erwin <afwyousenge@gmail.com>
2023-02-01 16:49:07 +0800

    add css in the first component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import { directive } from "@babel/types";
+import "./ExpenseItem.css";
 function ExpenseItem() {
-  return <h2>Expense Item!</h2>
+  return (
+    <div className="expense-item">
+      <div> Sep 9th 2023</div>
+      <div className="expense-item__discription">
+        <h2>Buy cheese</h2>
+        <div className="expense-item__prize">526.35</div>
+      </div>
+    </div>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
